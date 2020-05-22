7c1145dcc36fcc9fa2df0edef644234a0bd2b13f
Ajeet yadav <ajeetyadav164@gmail.com>
2020-05-22 16:52:12 +0530

    Quiz Notification Channel Added

--- DigiBoard/app/src/main/java/com/ak47/digiboard/application/App.java
+++ DigiBoard/app/src/main/java/com/ak47/digiboard/application/App.java
@@ -1,24 +1,44 @@
 package com.ak47.digiboard.application;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.util.Log;
 
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Picasso;
 
 public class App extends Application {
+    private static final String TAG = "App ";
+    String notificationChannelIdQuizAlert = "1000";
+    String notificationChannelNameQuizAlert = "Quiz Notification"; //Quiz Alert
     @Override
     public void onCreate() {
-        super.onCreate();
-
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
 
-//        load image offline - picasso library
-        Picasso.Builder builder = new Picasso.Builder(this);
+        Picasso.Builder builder = new Picasso.Builder(this);   // load image offline - picasso library
         builder.downloader(new OkHttp3Downloader(this, Integer.MAX_VALUE));
         Picasso built = builder.build();
 //        built.setIndicatorsEnabled(true);
         built.setLoggingEnabled(true);
         Picasso.setSingletonInstance(built);
+        createNotificationChannelForQuizAlert();
+        super.onCreate();
+    }
+
+    private void createNotificationChannelForQuizAlert() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(notificationChannelIdQuizAlert, notificationChannelNameQuizAlert, NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(notificationChannel);
+            Log.d(TAG, "Notification Channel Created");
+        }
     }
 }
